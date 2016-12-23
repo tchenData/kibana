ae533aa5620df4751c7c4203597c302b607cd0fe
tchenData <tchenData@users.noreply.github.com>
2016-12-23 15:15:47 +0800

    国际化支持
    使用 angular-translate

--- webpackShims/ui-bootstrap.js
+++ webpackShims/ui-bootstrap.js
@@ -3,9 +3,26 @@ define(function (require) {
   require('ui/angular-bootstrap/index');
 
   return require('ui/modules')
-  .get('kibana', ['ui.bootstrap'])
+  .get('kibana', ['ui.bootstrap', 'pascalprecht.translate'])
   .config(function ($tooltipProvider) {
     $tooltipProvider.setTriggers({ 'mouseenter': 'mouseleave click' });
+  })
+  .config(function ($translateProvider, $translatePartialLoaderProvider) {
+  $translateProvider.useLoader('$translatePartialLoader', {
+    urlTemplate: '{part}/i18n/{lang}.json'
   });
+  $translateProvider
+    .uniformLanguageTag('bcp47')
+    .registerAvailableLanguageKeys(['cn', 'en', 'de'], {
+    		'cn*': 'cn',
+        'en*': 'en',
+        'de*': 'de',
+        '*': 'cn'
+    })
+    .determinePreferredLanguage()
+    .fallbackLanguage('cn');
+})
+  ;
 
 });
+
